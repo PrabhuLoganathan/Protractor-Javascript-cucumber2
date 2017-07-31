6965aa3538f195968342f8796e70476e3d4a566c
Daniel Zen <daniel@zen.digital>
2017-07-31 15:14:02 -0400

    Updated Jasmine example, and eslint no-unused-vars to "warn"

--- jasmine/spec.js
+++ jasmine/spec.js
@@ -7,3 +7,22 @@ describe('A suite is just a function', function () {
     expect(a).toBe(true);
   });
 });
+
+describe('add',function(){
+  it('should be able to add 2 numbers',function(){
+    expect(add(2,3)).toBe(5);
+  });
+
+  it('should be able to add 2 MORE numbers',function(){
+    expect(add(5,3)).toBe(8);
+  });
+
+  it('should be able to add -ve numbers',function(){
+    expect(add(-5,8)).toBe(3);
+  });
+
+  it('should be able to add THREE numbers',function(){
+    expect(add(5,3,5)).toBe(13);
+  });
+
+});
